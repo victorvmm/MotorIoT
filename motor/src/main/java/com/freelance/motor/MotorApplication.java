27703afafa4793048783539bd538d2f924ea1deb
victor <victorvmm.michalany@gmail.com>
2026-08-28 16:19:07 -0300

    Adicionando initializer do spring, criar db

---
+++ motor/src/main/java/com/freelance/motor/MotorApplication.java
@@ -0,0 +1,13 @@
+package com.freelance.motor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorApplication.class, args);
+	}
+
+}
