ef3176bbc3176962d6b58e02a7556d25160ac7c1
victor <victorvmm.michalany@gmail.com>
2026-08-31 22:43:11 -0300

    Notification controller done, start service and repository layers.

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ To-do list:
     * service: services (queue data, telemetry rule)
     * worker: queue messages, perform HTTP calls
 
- - Create notifications and telemetry endpoints
+ - Add service and repository layer
+ - Add exceptions
  - Add RabbitMQ to compose
  - Implement worker + update docker-compose with MQ
  - Learn about DLQ and implement
--- motor/src/main/java/com/freelance/motor/controller/NotificationController.java
+++ motor/src/main/java/com/freelance/motor/controller/NotificationController.java
@@ -1,10 +1,23 @@
 package com.freelance.motor.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.freelance.motor.dto.NotificationDTO;
+
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/v1/notification")
 public class NotificationController {
+    
+    @PostMapping
+    public ResponseEntity<Void> receiveNotification(@Valid @RequestBody NotificationDTO request) {
+        return ResponseEntity.accepted().build();
+    }
     
 }
